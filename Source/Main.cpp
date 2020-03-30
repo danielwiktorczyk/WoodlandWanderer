c93551f6004ca748307b4c600cb3c81c744821e0
Daniel <danielwiktorczyk@gmail.com>
2020-03-30 17:18:56 -0400

    Using base of Daniel's A2

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,4 +1,5 @@
 #include "../Include/Main.h"
+#include "../VS2017/OBJloader.h"
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
@@ -15,47 +16,83 @@ glm::vec3 cameraPos = glm::vec3(0.0f, 0.1f, 1.5f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-int main() {
+GLuint setupModelVBO(std::string path, int& vertexCount) {
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec2> UVs;
 
+	//read the vertex data from the model's OBJ file
+	loadOBJ(path.c_str(), vertices, normals, UVs);
+
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO); //Becomes active VAO
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+
+	//Vertex VBO setup
+	GLuint vertices_VBO;
+	glGenBuffers(1, &vertices_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, vertices_VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Normals VBO setup
+	GLuint normals_VBO;
+	glGenBuffers(1, &normals_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, normals_VBO);
+	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+
+	//UVs VBO setup
+	GLuint uvs_VBO;
+	glGenBuffers(1, &uvs_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, uvs_VBO);
+	glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs, as we are using multiple VAOs)
+	vertexCount = vertices.size();
+	return VAO;
+}
+
+int main() {
+	// Initialize GLFW and OpenGL version
 	glfwInit();
 
-#if defined(PLATFORM_OSX)	
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2); 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#else
+#if defined(PLATFORM_OSX)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#else
+	// On windows, we set OpenGL version to 2.1, to support more hardware
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(1600, 1200, "Comp371 - Project", NULL, NULL);
-    if (window == NULL) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+	// Create Window and rendering context using GLFW, resolution is 1024x768
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Comp371 - Project", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-    glfwMakeContextCurrent(window);
-    glfwSetCursorPosCallback(window, mouseCallback);
-    glfwSetScrollCallback(window, scrollCallback);
+	// Disable mouse cursor when over the window!
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    //capture initial mouse position
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    
-    // Initialize GLEW
-    glewExperimental = true;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to create GLEW" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glEnable(GL_CULL_FACE);
-
-    glEnable(GL_DEBUG_OUTPUT);
-    glDebugMessageCallback(MessageCallback, 0);
+	// Initialize GLEW
+	glewExperimental = GL_TRUE; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to create GLEW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
     
 	//----------------------------------------------------------------------
 	//----------------------------- Shaders --------------------------------
@@ -67,8 +104,9 @@ int main() {
 	//----------------------------- Objects --------------------------------
 	//----------------------------------------------------------------------
 
-    EnvironmentPlane env = EnvironmentPlane();
-	LoadedObject grass = LoadedObject("../Assets/simple-grass.obj");
+    std::string treePath = "../Assets/low-poly-trees-pack.obj";
+    int treeVertices;
+    GLuint treeVAO = setupModelVBO(treePath, treeVertices);
     
 	//----------------------------------------------------------------------
 	//------------------------------ Render --------------------------------
@@ -83,8 +121,13 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT);
      		
-		env.draw(shader);
-		grass.draw(shader, { 0.89f, 0.9f, 0.42f });
+		// Tree!
+		glBindVertexArray(0);
+		glBindVertexArray(treeVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, treeVAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, treeVertices);
+
 
         glfwSwapBuffers(window); 
         glfwPollEvents();
@@ -213,3 +256,5 @@ void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum se
     std::cout << std::endl;
     std::cout << "---------------------opengl-callback-end--------------" << std::endl;
 };
+
+
