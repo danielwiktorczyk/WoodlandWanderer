eac6a1818b986991256a3c98eadee96480caf729
Unknown <claudia.lapalme@gmail.com>
2020-04-04 23:30:53 -0400

    disable face culling for models
    some of the loaded models lose half of their triangles when culling is turned on. We have no choice but to turn it off if we dont want to waste too much time

--- Source/DrawableModel.cpp
+++ Source/DrawableModel.cpp
@@ -3,6 +3,7 @@
 Model::Model(const GLuint& VAO, const GLuint& numVertices, glm::vec3 color) : numVertices(numVertices), color(color), VAO(VAO) { }
 
 void Model::draw(GLuint& worldMatrixLocation, GLuint& colorLocation) {
+	glDisable(GL_CULL_FACE);
 	
 	glBindVertexArray(this->VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, this->VAO);
@@ -11,4 +12,5 @@ void Model::draw(GLuint& worldMatrixLocation, GLuint& colorLocation) {
 	glUniform3fv(colorLocation, 1, value_ptr(this->color));
 	
 	glDrawArrays(GL_TRIANGLES, 0, this->numVertices);
+	glEnable(GL_CULL_FACE);
 }
